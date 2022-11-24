bf96c40e47fb9511100cd8ee9b25a8b7c586e58f
ht93084 <ht93084@gmail.com>
2022-11-24 22:52:40 +0800

    feat: 1.輸入框基礎設定 2.聊天視窗msg顯示功能

--- app.js
+++ app.js
@@ -1,75 +1,65 @@
-const notifier = require('node-notifier')
-const path = require('path')
-const moment = require('moment')
-const remote = require('electron').remote
+const messages = document.querySelector('#messages')
 
-const elNow = document.querySelector('.now-time')
-const elAlarm = document.querySelector('.alarm-time')
-elAlarm.addEventListener('change', onAlarmTextChange)
+const enterA = document.querySelector('#enter-A')
+const nameA = document.querySelector('#name-A')
+const textareaA = document.querySelector('#textarea-A')
 
-let time = moment()
+enterA.addEventListener('click', ContentEnter)
 
-let nowTime
-let alarmTime
+function ContentEnter() {
 
-/** Set Time */
-const now = moment(time).format('HH:mm:ss')
-nowTime = now
-elNow.innerText = now
+    // 如果未輸入
+    if (textareaA.value === '') return;
 
-const alarm = moment(time).add(5, 'seconds').format('HH:mm:ss')
-alarmTime = alarm
-elAlarm.value = alarm
+    // 對話紀錄顯示
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${nameA.value}：${textareaA.value}`));
+    li.setAttribute('class', 'msg-block msg-block-B');
+    messages.appendChild(li);
 
-timer()
+    messages.scrollTop = messages.scrollHeight;
 
-/** Now Time */
-function timer() {
-    time = moment().format('HH:mm:ss')
-
-    /** Set Now */
-    nowTime = time
-    elNow.innerText = time
-
-    check()
-
-    setTimeout(() => {
-        timer()
-    }, 1000)
+    // 復原輸入框
+    textareaA.value = '';
 }
 
-/** Check Time */
-function check() {
-    const diff = moment(nowTime, 'HH:mm:ss').diff(moment(alarmTime, 'HH:mm:ss'))
-    if (diff === 0) {
-        notice(`It's ${alarmTime}. Wake Up!`)
-    }
-}
+// on指令：當接收到server傳來的
+// const notifier = require('node-notifier')
+// const path = require('path')
+// const moment = require('moment')
+// const remote = require('electron').remote
 
-/**
- * System Notification
- * @param {string} msg
- */
-function notice(msg) {
-    /** Show Form */
-    const window = remote.getCurrentWindow()
-    window.restore()
-    window.show()
+// const elNow = document.querySelector('.now-time')
+// const elAlarm = document.querySelector('.alarm-time')
+// elAlarm.addEventListener('change', onAlarmTextChange)
 
-    /** https://github.com/mikaelbr/node-notifier */
-    notifier.notify({
-        title: 'Alarm Clock',
-        message: msg,
-        icon: path.join(__dirname, 'clock.ico'),
-        sound: true,
-    })
-}
+// let time = moment()
 
-/**
- * Save To Global Variable,
- * Can't Read Dom In Minimize Status.
- * @param {event} event
- */
-function onAlarmTextChange(event) {
-    alarmTime = event.target.value
-}
+// let nowTime
+// let alarmTime
+
+// /** Set Time */
+// const now = moment(time).format('HH:mm:ss')
+// nowTime = now
+// elNow.innerText = now
+
+// const alarm = moment(time).add(5, 'seconds').format('HH:mm:ss')
+// alarmTime = alarm
+// elAlarm.value = alarm
+
+// timer()
+
+// /** Now Time */
+// function timer() {
+//     time = moment().format('HH:mm:ss')
+
+//     /** Set Now */
+//     nowTime = time
+//     elNow.innerText = time
+
+//     check()
+
+//     setTimeout(() => {
+//         timer()
+//     }, 1000)
+// }
