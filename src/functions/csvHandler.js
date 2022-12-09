af47292dbabfee2d0670028ee5b82402771a8403
qmsiteandy <ht93084@gmail.com>
2022-12-09 09:34:11 +0800

    chore: 整理程式

--- src/functions/csvHandler.js
+++ src/functions/csvHandler.js
@@ -5,22 +5,16 @@ var newLine = '\r\n';
 var fields = ['名稱', '時間', '內容'];
 
 
-// appendDataToFile(os.homedir() + '/Desktop/file.csv', '1,2,3')
-//     .catch(err => {
-//         // console.log(err)
-//         if (err.stack.includes('EBUSY')) {
-//             alert('請確認 CSV 檔關閉，否則影響資料儲存');
-//         }
-//     })
-
 async function appendDataToFile(path, data) {
 
+    console.log(path)
+    
     // console.log(path)
     fs.stat(path, function (err, stat) {
         if (err != null) {
 
             // 建立 file + fields + 新row
-            console.log('New file, just writing headers');
+            // console.log('New file, just writing headers');
 
             fs.writeFile(path, '\ufeff' + fields + newLine + data + newLine, 'utf-8', (err2) => {
                 // if (err2) throw err2;
@@ -30,9 +24,9 @@ async function appendDataToFile(path, data) {
         }
     });
 
-    console.log('append file')
+    // console.log('append');
     await fs.promises.appendFile(path, '\ufeff' + data + newLine, 'utf-8');
-
+    
     return true;
 }
 
